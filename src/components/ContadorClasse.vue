801074629c4f38826ecdec3810495977aad32dd8
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2023-09-14 18:47:55 -0300

    Full and only commit

---
+++ src/components/ContadorClasse.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Contador (Classe)</h1>
+    <h2>{{ valor }}</h2>
+    <h3>{{ parOuImpar }}</h3>
+    <button @click="setValor(-1)">Dec</button>
+    <button @click="setValor(1)">Inc</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue, prop } from "vue-class-component";
+
+class Props {
+  valorInicial = prop({
+    type: Number,
+    default: 0,
+    required: true,
+  });
+}
+
+@Options({})
+export default class ContadorClasse extends Vue.with(Props) {
+  valor = this.valorInicial;
+
+  public setValor(delta: number) {
+    this.valor += delta;
+  }
+
+  public getParOuImpar(): string {
+    return this.valor % 2 === 0 ? "Par" : "Impar";
+  }
+
+  public get parOuImpar(): string {
+    return this.valor % 2 === 0 ? "Par" : "Impar";
+  }
+}
+</script>
+
+<style></style>
